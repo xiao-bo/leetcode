2402d4fb8594137e65a298e69439dd73c39ee627
xiao-bo <ay852852@gmail.com>
2020-02-22 13:51:16 +0800

    brute method

--- easy/c/twosum.c
+++ easy/c/twosum.c
@@ -1,23 +1,26 @@
 #include<stdio.h>
 #include<stdlib.h>
-int* twoSum(int* nums, int numsSize, int target, int* returnSize){
-    
+int* twoSum(int* nums, int numsSize, int target){
+    int *returnSize = (int*)malloc(2*sizeof(int));
+
     for (int i = 0; i < numsSize;i++){
 
-        for (int j = i+1; i < numsSize ; i++){
+        for (int j = i+1; j < numsSize ; j++){
             if ((nums[i]+nums[j]) == target){
                 returnSize[0] = i;
                 returnSize[1] = j;
+               
             }
         }
     }
+    
+    
     return returnSize;
 }
 
 int main(){
-    int *nums[4] = {2, 7, 11, 15};
-    int len = 4;
-    int *ans;
-    ans = twoSum(nums,4,9,ans);
-    printf("%p",ans);
+    int nums[4] = {2, 7, 11, 15};    
+    int *ans = twoSum(nums,4,9);
+    printf("%d %d",ans[0],ans[1]);
+    free(ans);
 }
