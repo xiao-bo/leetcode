bf66a96962afc13d651751b0580442af68ded78a
xiao-bo <ay852852@gmail.com>
2021-08-22 15:51:55 +0800

    feature: 本來想用set的邏輯去解，但發現遇到[3,3]的狀況，set會失敗

--- easy/twoSum/twoSum2.py
+++ easy/twoSum/twoSum2.py
@@ -2,7 +2,7 @@ def get_two_sum(nums, target):
 
     if len(nums) < 2:
         return []
-
+    '''
     # brute method after work 1 years
     # Runtime: 4016 ms, faster than 24.64% of Python3 online submissions for Two Sum.
     # Memory Usage: 14.6 MB, less than 98.26% of Python3 online submissions for Two Sum.
@@ -10,3 +10,16 @@ def get_two_sum(nums, target):
         for y in range(x + 1, len(nums)):
             if nums[x] + nums[y] == target:
                 return [x, y]
+    '''
+    # set 
+    set_nums = set(nums)
+    for x in nums:
+        if target - x in set_nums:
+            x_index = get_index(nums, x)
+            y_index = get_index(nums, target - x, x_index)
+            return [x_index, y_index]
+
+def get_index(nums, x, exclude_index=None):
+    for index in range(0, len(nums)):
+        if nums[index] == x and index != exclude_index:
+            return index
