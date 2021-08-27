53987ce1fa1f26f2fc6390e891c118d5e56f9ff8
xiao-bo <ay852852@gmail.com>
2021-08-27 23:05:10 +0800

    feature:補完測試

--- easy/twoSumIV/testTwoSumIV.py
+++ easy/twoSumIV/testTwoSumIV.py
@@ -1,23 +1,36 @@
 from twoSumIV import findTarget
-
+from twoSumIV import bst_insert
+from twoSumIV import TreeNode
 
 def test_findTarget():
-
-    root = [5,3,6,2,4,None,7]
+    
+    nums = [5,3,6,2,4,None,7]
     k = 9
+    bst = TreeNode(nums[0])
+    for x in range(1, len(nums)):
+        bst_insert(bst, TreeNode(nums[x]))
 
-    assert findTarget(root, k) == True
+    assert findTarget(bst, 9) == True
+    assert findTarget(bst,28) == False
+    assert findTarget(bst,10) == True
 
-    root = [5,3,6,2,4,None,7]
-    k = 28
-
-    assert findTarget(root,k) == False
-
-    root = [2,1,3]
+    nums = [2,1,3]
     k = 4
+    bst = TreeNode(nums[0])
+    for x in range(1, len(nums)):
+        bst_insert(bst, TreeNode(nums[x]))
 
-    assert findTarget(root, 4) == True
+    assert findTarget(bst, 4) == True
 
-    assert findTarget(root, 1) == False
+    assert findTarget(bst, 1) == False
 
-    assert findTarget(root, 3) == True
+    assert findTarget(bst, 3) == True
+    
+    nums = [1]
+    k = 2
+    bst = TreeNode(nums[0])
+    for x in range(1, len(nums)):
+        bst_insert(bst, TreeNode(nums[x]))
+
+    assert findTarget(bst, k) == False
+    
