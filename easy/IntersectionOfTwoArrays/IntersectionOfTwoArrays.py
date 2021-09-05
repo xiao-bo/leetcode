a0f205579813d9f47c46b6db95b7b9c5c0ac96be
xiao-bo <ay852852@gmail.com>
2021-09-05 15:41:25 +0800

    feature: 使用hash table解題，並且邊刪除num2相同的元素，並免重複搜尋

--- easy/IntersectionOfTwoArrays/IntersectionOfTwoArrays.py
+++ easy/IntersectionOfTwoArrays/IntersectionOfTwoArrays.py
@@ -9,9 +9,20 @@ class Solution:
         # method 1 using built-in function. 
         # Runtime: 48 ms, faster than 55.21% of Python3 online submissions for Intersection of Two Arrays.
         # Memory Usage: 14.5 MB, less than 46.88% of Python3 online submissions for Intersection of Two Arrays.
-        result = list(set(nums1).intersection(set(nums2)))
+        #result = list(set(nums1).intersection(set(nums2)))
 
 
+        # method 2 using hash table
+        # Runtime: 64 ms, faster than 22.62% of Python3 online submissions for Intersection of Two Arrays.
+        # Memory Usage: 14.2 MB, less than 91.32% of Python3 online submissions for Intersection of Two Arrays.
+        result = set()
+        for x in nums1:
+            if x in nums2:
+                result.add(x)
+                nums2.remove(x)
+        result = list(result)
+
+        #         
         return result
         
 
