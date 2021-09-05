a2c426de3d5bb912fb2916c0e5b2a37c5257ed5f
xiao-bo <ay852852@gmail.com>
2021-09-05 15:40:13 +0800

    feature: 補完空值的測試

--- easy/IntersectionOfTwoArrays/testInterestionOfTwoArrays.py
+++ easy/IntersectionOfTwoArrays/testInterestionOfTwoArrays.py
@@ -1,11 +1,26 @@
 from IntersectionOfTwoArrays import Solution
 
 
-def test_interestion():
+def test_intersection():
     s = Solution()
+    # check null
+    nums1 = []
+    nums2 = [2,2]
+    result = s.intersection(nums1, nums2)
+    expected_result = False
+
+    assert result == expected_result
+
+    nums1 = [1,2]
+    nums2 = [] 
+    result = s.intersection(nums1, nums2)
+    expected_result = False
+
+    assert result == expected_result
+
     nums1 = [1,2,2,1]
     nums2 = [2,2]
-    result = s.interestion(nums1, nums2)
+    result = s.intersection(nums1, nums2)
     expected_result = [2]
 
     assert result == expected_result
@@ -13,8 +28,9 @@ def test_interestion():
     nums1 = [4,9,5]
     nums2 = [9,4,9,8,4]
 
-    result = s.interestion(nums1, nums2)
+    result = s.intersection(nums1, nums2)
+    result.sort()
     expected_result = [4,9]
 
-    assert result.sorted() == expected_result
+    assert result == expected_result
 
