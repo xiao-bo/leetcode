fe3a583b3c87c93acb29578d8ae4fddfcfb07e7e
xiao-bo <ay852852@gmail.com>
2021-09-05 16:01:54 +0800

    feature: 提昇第四個方法，先使用set，先移除重複的元素，再開始比較，最後轉回listr，這個方法贏過92.5%的人

--- easy/IntersectionOfTwoArrays/IntersectionOfTwoArrays.py
+++ easy/IntersectionOfTwoArrays/IntersectionOfTwoArrays.py
@@ -31,6 +31,7 @@ class Solution:
         # method 4 improve method2 
         # Runtime: 56 ms, faster than 33.52% of Python3 online submissions for Intersection of Two Arrays.
         # Memory Usage: 14.3 MB, less than 91.32% of Python3 online submissions for Intersection of Two Arrays.
+        '''
         if len(nums1) > len(nums2):
             small_list = nums2
             large_list = nums1
@@ -43,6 +44,26 @@ class Solution:
             if x in large_list:
                 result.add(x)
                 large_list.remove(x)
+        result = list(result)
+        '''
+        # method 5 improve method 4 by remove duplicated element firstly depend on set
+        # Runtime: 40 ms, faster than 92.51% of Python3 online submissions for Intersection of Two Arrays.
+        # Memory Usage: 14.4 MB, less than 46.88% of Python3 online submissions for Intersection of Two Arrays.
+
+        set1 = set(nums1)
+        set2 = set(nums2)
+        if len(set1) > len(set2):
+            small_set = set2
+            large_set = set1
+        else:
+            small_set = set1
+            large_set = set2
+
+        result = set()
+        for x in small_set:
+            if x in large_set:
+                result.add(x)
+                large_set.remove(x)
         result = list(result)
 
         return result
