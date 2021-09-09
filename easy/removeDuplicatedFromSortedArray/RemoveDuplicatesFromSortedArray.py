cc56f87f935b5ba814b62066571b2ae3b28e4fd3
xiao-bo <ay852852@gmail.com>
2021-09-09 23:56:31 +0800

    feature: 本來想優化，降低重複尋找的次數， [0,0,0,1,2,3] -> [0,1,2,3,2,3] 原先想法是會找六次 想優化成，找到第一個3(index = 4）時就停下來 但這時候才發現，python list是動態長度，所以 nums[i+1:] = nums[j:] 已經會將 [0,0,0,1,2,3] -> [0,1,2,3] 將後面的2,3砍掉

--- easy/removeDuplicatedFromSortedArray/RemoveDuplicatesFromSortedArray.py
+++ easy/removeDuplicatedFromSortedArray/RemoveDuplicatesFromSortedArray.py
@@ -1,26 +1,43 @@
 from typing import List
 
-#https://leetcode.com/problems/remove-duplicates-from-sorted-array/submissions/
+# https://leetcode.com/problems/remove-duplicates-from-sorted-array/submissions/
+
+
 class Solution:
 
-    # method 1 using two index 
+    # method 1 using two index
     # Runtime: 134 ms, faster than 30.22% of Python3 online submissions for Remove Duplicates from Sorted Array.
     # Memory Usage: 15.8 MB, less than 7.08% of Python3 online submissions for Remove Duplicates from Sorted Array.
     def removeDuplicates(self, nums: List[int]) -> int:
-        #nums = [1,2]
         print(f'len = {len(nums)}')
         i = 0
+        remove_count = 0
         while i < len(nums):
             j = i + 1
             while j < len(nums):
                 if nums[i] == nums[j]:
-                    j = j+1
+                    j = j + 1
                 else:
                     break
-                    
-            nums[i+1:]=nums[j:]
+            print(f'i = {i}, j = {j},nums={nums} ')
+            #print(f'nums[{i+1}:] = {nums[i+1]}')
+            print(f'nums[{j}:] = {nums[j:]}')
+            nums[i+1:] = nums[j:]
+            remove_count = remove_count + j - i 
+            print(nums)
+            
             i = i + 1
 
-        print(f'i = {i} nums[:{i}] = {nums[:i]}')
-        #print(nums[:i])
+
+        #print(f'i = {i} nums[:{i}] = {nums[:i]}')
+        # print(nums[:i])
         return (len(nums[:i]), nums[:i])
+
+s = Solution()
+nums = [1, 1, 2]
+res_k, res_nums = s.removeDuplicates(nums)
+print(res_k, res_nums)
+
+nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+res_k, res_nums = s.removeDuplicates(nums)
+print(res_k, res_nums)
