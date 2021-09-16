91029d1ad9d8164657d9b4a517d318d3cc8d98d0
xiao-bo <ay852852@gmail.com>
2021-09-16 23:59:05 +0800

    feature:新增case

---
+++ easy/removeElement/testRemoveElement.py
@@ -0,0 +1,52 @@
+
+from removeElement import Solution
+
+def test_removeElement():
+    nums = [3,2,2,3]
+    val = 3
+    expected_result = [2,2]
+    s = Solution()
+    res_val, res_nums = s.removeElement(nums, val)
+    nums.sort()
+    expected_nums = []
+    assert res_val == len(expected_result)
+    for i in range(0, res_val):
+        assert res_nums[i] == expected_result[i]
+
+    nums = [0,1,2,2,3,0,4,2]
+    val = 2
+    expected_result = [0,0,1,4,3]
+    nums.sort()
+    res_val, res_nums = s.removeElement(nums, val)
+    assert res_val == len(expected_result)
+    for i in range(0, res_val):
+        assert res_nums[i] == expected_result[i]
+        print(i)
+
+    nums = [2]
+    val = 3
+    expected_result = [2]
+    nums.sort()
+    res_val, res_nums = s.removeElement(nums, val)
+    assert res_val == len(expected_result)
+    for i in range(0, res_val):
+        assert res_nums[i] == expected_result[i]
+
+    nums = [1]
+    val = 1
+    expected_result = []
+    nums.sort()
+    res_val, res_nums = s.removeElement(nums, val)
+    assert res_val == len(expected_result)
+    for i in range(0, res_val):
+        assert res_nums[i] == expected_result[i]
+
+    nums = [3,3]
+    val = 3
+    expected_result = []
+    nums.sort()
+    res_val, res_nums = s.removeElement(nums, val)
+    assert res_val == len(expected_result)
+    for i in range(0, res_val):
+        assert res_nums[i] == expected_result[i]
+
