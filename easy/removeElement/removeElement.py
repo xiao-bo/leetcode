06b2d50a5661e44878b88edde91a9f4ac83f6537
xiao-bo <ay852852@gmail.com>
2021-09-18 10:46:55 +0800

    feature: 重新盤一次，改寫swap_count成目標element的數量，就解決囉

--- easy/removeElement/removeElement.py
+++ easy/removeElement/removeElement.py
@@ -2,26 +2,35 @@ from typing import List
 
 class Solution:
 
-
+    # method 1 
+    # Runtime: 36 ms, faster than 53.11% of Python3 online submissions for Remove Element.
+    # Memory Usage: 14.1 MB, less than 92.12% of Python3 online submissions for Remove Element.
     def removeElement(self, nums: List[int], val: int) -> int:
-        swap_count = 0 # swap_count of duplicate element
+        count_of_remove_element = 0 
         head = 0 # head index
         tail = len(nums) - 1  # tail index
 
-        # if head == tail:
-        #     if nums[head] != 
-        #     return (1,nums)
+        if head == tail:
+            print(nums[head], val)
+            if val != nums[head]:
+                print('!=')
+                return (1, nums)
+            else:
+                print('==')
+                return (0,[])
 
-        while head < tail :
+        while head <= tail :
             #print(nums)
             if nums[head] == val and nums[tail] != val:
                 nums[head],nums[tail] = nums[tail],nums[head] # swap
-                swap_count = swap_count + 1
             if nums[head] != val:
                 head = head + 1
             if nums[tail] == val:
                 tail = tail - 1 
+                count_of_remove_element = count_of_remove_element + 1
                 
-        print(f'swap_count = {swap_count} nums[:{-swap_count-1}] = {nums[:-swap_count-1]}')
-        #print(nums[:-swap_count-1])
-        return (len(nums) - swap_count-1, nums[:-swap_count-1])
+        print(f'count_of_remove_element = {count_of_remove_element} '
+              f'nums[:{-count_of_remove_element}] = '
+              f'{nums[:-count_of_remove_element]}')
+        #print(nums[:-count_of_remove_element-1])
+        return (len(nums) - count_of_remove_element, nums[:-count_of_remove_element])
