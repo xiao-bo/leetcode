f22f0596468b8b263d1cac7d15b26aa290c91f81
xiao-bo <ay852852@gmail.com>
2021-09-23 21:18:53 +0800

    feature:補充測資，以及寫完第一個方法，但速度偏低

--- easy/bestTimeToBuyAndSellStock/bestTimetoBuyandSellStock.py
+++ easy/bestTimeToBuyAndSellStock/bestTimetoBuyandSellStock.py
@@ -1,5 +1,28 @@
 from typing import List
+#https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 
 class Solution:
+
+    # method1 using two pointer
+    # Runtime: 1144 ms, faster than 38.06% of Python3 online submissions for Best Time to Buy and Sell Stock.
+    # Memory Usage: 25 MB, less than 95.38% of Python3 online submissions for Best Time to Buy and Sell Stock.
     def maxProfit(self, prices: List[int]) -> int:
-        return 1
+
+        if len(prices) == 1:
+            return 0
+        if not prices:
+            return 0
+        low = 0 
+        high = 1
+        max_profit = 0
+
+        while high < len(prices):
+            if prices[low] > prices[high]:
+                low = high 
+            else:
+                current_max = prices[high] - prices[low] 
+                max_profit = max(current_max, max_profit)
+
+            high = high + 1
+
+        return max_profit
--- easy/bestTimeToBuyAndSellStock/testBestTimetoBuyandSellstock.py
+++ easy/bestTimeToBuyAndSellStock/testBestTimetoBuyandSellstock.py
@@ -1,13 +1,39 @@
 from bestTimetoBuyandSellStock import Solution
 
 def testMaxProfit():
-	s = Solution()
-	prices = [7,1,5,3,6,4]
-	result = s.maxProfit(prices)
+    s = Solution()
+    prices = [7,1,5,3,6,4]
+    result = s.maxProfit(prices)
 
-	assert result == 5 
+    assert result == 5 
 
-	prices = [7,6,4,3,1]
-	result = s.maxProfit(prices)
+    prices = [7,6,4,3,1]
+    result = s.maxProfit(prices)
 
-	assert result == 0
+    assert result == 0
+
+    prices = [7,8,100,1,2]
+    result = s.maxProfit(prices)
+
+    assert result == 93
+
+    prices = [7,6,100,1,2]
+    result = s.maxProfit(prices)
+
+    assert result == 94
+
+
+    prices = [7,8,100,1,101]
+    result = s.maxProfit(prices)
+
+    assert result == 100
+
+    prices = []
+    result = s.maxProfit(prices)
+
+    assert result == 0
+
+    prices = [1]
+    result = s.maxProfit(prices)
+
+    assert result == 0
