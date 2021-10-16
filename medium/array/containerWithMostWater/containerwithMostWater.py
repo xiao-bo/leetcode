259c98e794495611c22fd9da5c049cd4f11736b7
xiao-bo <ay852852@gmail.com>
2021-10-16 16:26:58 +0800

    feature:新增第四個方法：優化第三個方法，如果current head比最左邊的head還小的話，就跳過，因為 current head小，就不可能找到更大的水量
    故current tail也是同理，比最右邊的tail小就跳過，藉此降低計算水量的次數

--- medium/array/containerWithMostWater/containerwithMostWater.py
+++ medium/array/containerWithMostWater/containerwithMostWater.py
@@ -12,7 +12,24 @@ class Solution:
 
         return max_water
         '''
-        # method2,  head and tail pointer
+        
+        #method 2, improve brute method
+        '''
+        max_water = 0 
+        for x in range(0, len(height)):
+            for y in range(x+1, len(height)):
+                if height[y] < height[-1]:
+                    continue
+                if height[x] < height[0]:
+                    continue
+                    
+                current_water = self.find_current_water(height, x, y)
+                max_water = max(current_water, max_water)
+
+        return max_water
+        ''' 
+
+        # method3,  head and tail pointer
         # Runtime: 1218 ms, faster than 9.44% of Python3 online submissions for Container With Most max_water.
         # Memory Usage: 27.7 MB, less than 22.84% of Python3 online submissions for Container With Most max_water.
         '''
@@ -34,16 +51,36 @@ class Solution:
 
         return max_water
         '''
-        '''
-        method 3, improve brute method
-        '''
+        # method4,  improve method 3
+        # Runtime: 790 ms, faster than 40.24% of Python3 online submissions for Container With Most Water.
+        # Memory Usage: 27.4 MB, less than 76.80% of Python3 online submissions for Container With Most Water.
+        
+        head = 0 
+        tail = len(height) - 1 
         max_water = 0 
-        for x in range(0, len(height)):
-            for y in range(x+1, len(height)):
-                current_water = self.find_current_water(height, x, y)
-                max_water = max(current_water, max_water)
+        count = 0
+        while head < tail:
+            if height[head] < height[0]:
+                # current head is smaller first head
+                head = head + 1
+                continue
+            if height[tail] < height[-1]:
+                tail = tail - 1 
+                continue
+
+            current_water = self.find_current_water(height, head, tail)
+            max_water = max(max_water, current_water)
+            print(f'head = {head}, tail = {tail}, ')
+            print(f'current_water = {current_water}')
+
+            if height[tail] > height[head]:
+                head = head + 1
+            else:
+                tail = tail -1 
+        
 
         return max_water
+        
 
     def find_current_water(self,height, head, tail):
         return (tail - head) * min(height[head], height[tail])
