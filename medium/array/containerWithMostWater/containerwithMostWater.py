91a7cc2a6f54c5c379748505eb2700f8248a0d3c
xiao-bo <ay852852@gmail.com>
2021-10-16 16:18:07 +0800

    feature:將面積改名成water，更直白

--- medium/array/containerWithMostWater/containerwithMostWater.py
+++ medium/array/containerWithMostWater/containerwithMostWater.py
@@ -4,37 +4,48 @@ class Solution:
     def maxArea(self, height: List[int]) -> int:
         '''
         # brute method 
-        total_max = 0 
+        max_water = 0 
         for x in range(0, len(height)):
             for y in range(x+1, len(height)):
-                current_max = min(height[x], height[y]) * (y - x)
-                total_max = max(current_max, total_max)
+                current_water = min(height[x], height[y]) * (y - x)
+                max_water = max(current_water, max_water)
 
-        return total_max
+        return max_water
         '''
         # method2,  head and tail pointer
-        # Runtime: 1218 ms, faster than 9.44% of Python3 online submissions for Container With Most Water.
-        # Memory Usage: 27.7 MB, less than 22.84% of Python3 online submissions for Container With Most Water.
-        
+        # Runtime: 1218 ms, faster than 9.44% of Python3 online submissions for Container With Most max_water.
+        # Memory Usage: 27.7 MB, less than 22.84% of Python3 online submissions for Container With Most max_water.
+
         head = 0 
         tail = len(height) - 1 
-        total_max_area = 0 
+        max_water = 0 
         count = 0
         while head < tail:
-            current_max_area = self.find_current_area(height, head, tail)
-            total_max_area = max(total_max_area, current_max_area)
+            current_water = self.find_current_water(height, head, tail)
+            max_water = max(max_water, current_water)
             print(f'head = {head}, tail = {tail}, ')
-            print(f'current_max_area = {current_max_area}')
+            print(f'current_water = {current_water}')
 
             if height[tail] > height[head]:
                 head = head + 1
             else:
                 tail = tail -1 
-            
+        
 
-        return total_max_area
+        return max_water
 
-    def find_current_area(self,height, head, tail):
+        '''
+        method 3, improve brute method
+        '''
+        max_water = 0 
+        for x in range(0, len(height)):
+            for y in range(x+1, len(height)):
+                current_water = min(height[x], height[y]) * (y - x)
+                max_water = max(current_water, max_water)
+
+        return max_water
+
+    def find_current_water(self,height, head, tail):
         return (tail - head) * min(height[head], height[tail])
 
         
