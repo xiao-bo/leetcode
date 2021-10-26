c4be64714ae5130f44705a1ea5cb017f1c5c6da4
xiao-bo <ay852852@gmail.com>
2021-10-26 23:27:36 +0800

    feature:想到第二個解法，但移除重複資料的時間複雜度還是太高，我猜是因為型態一直轉換的關係，那有什麼方法可以移除重複資料呢？

--- medium/array/3sum/testThreeSum.py
+++ medium/array/3sum/testThreeSum.py
@@ -8,6 +8,9 @@ def testThreeSum():
     expected_result = [[-1,-1,2],[-1,0,1]]
     result = s.threeSum(nums)
 
+    # 先檢查數量，以免兩邊長度不一
+    assert len(result) == len(expected_result)
+
     for x in expected_result:
         if x in result:
             continue
--- medium/array/3sum/threeSum_2021.py
+++ medium/array/3sum/threeSum_2021.py
@@ -10,7 +10,7 @@ class Solution:
 
         # brute method time complexity O(n^3)
         # Time Limit Exceeded
-        
+        '''
         for x in range(0, length):
             for y in range(x+1, length):
                 for z in range(y+1, length):
@@ -25,5 +25,23 @@ class Solution:
             result[x] = list(result[x])
 
         return result
+        '''
+        # method 2: reduce brute by a+b = -c, time complexity = O(n^2)
+        nums.sort()
+
+        for x in range(0, length):
+            for y in range(x+1, length):
+                if -(nums[x] + nums[y]) in nums[y+1:]:
+                    print(f'y = {y}, nums[y:] = {nums[y:]}')
+                    print(nums[x],nums[y],nums[x]+nums[y])
+                    element = [nums[x], nums[y], -(nums[x]+nums[y])]
+                    result.append(tuple(element))
+
+        result = list(set(result))
+        print(result)
         
-        # method 2: 
+        # remove depulicated element
+        for x in range(0,len(result)):
+            result[x] = list(result[x])
+        
+        return result
