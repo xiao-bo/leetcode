4f258758d7e5a732f0fc7d86d546a474fb5adb8a
xiao-bo <ay852852@gmail.com>
2021-10-31 22:06:47 +0800

    docs: 增加註解

--- medium/array/3sum/threeSum_2021.py
+++ medium/array/3sum/threeSum_2021.py
@@ -26,22 +26,28 @@ class Solution:
 
         return result
         '''
-        # method 2: reduce brute by a+b = -c, time complexity = O(n^2)
+        # method 2: reduce brute by a+b = -c, time complexity = O(n^2+N?) 
+        # took much time because transfer tuple to set and to list.
+        '''
         nums.sort()
-
+        x = 0 
+        y = length -1
         for x in range(0, length):
-            for y in range(x+1, length):
+            for y in range(x+1, length-1):
                 if -(nums[x] + nums[y]) in nums[y+1:]:
-                    print(f'y = {y}, nums[y:] = {nums[y:]}')
+                    print(f'x = {x} y = {y}, nums[y:] = {nums[y:]}')
                     print(nums[x],nums[y],nums[x]+nums[y])
                     element = [nums[x], nums[y], -(nums[x]+nums[y])]
-                    result.append(tuple(element))
-
-        result = list(set(result))
+                    result.append(element)
         print(result)
+        
+        result = list(set(result))
+        
         
         # remove depulicated element
         for x in range(0,len(result)):
             result[x] = list(result[x])
         
         return result
+        '''
+        # method 3 
