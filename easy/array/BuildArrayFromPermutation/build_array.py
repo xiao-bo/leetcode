583865edc20430a535fedf03fb96e2cfffcdf88f
xiao-bo <ay852852@gmail.com>
2022-05-26 21:44:23 +0800

    feature: using one line to solve

--- easy/array/BuildArrayFromPermutation/build_array.py
+++ easy/array/BuildArrayFromPermutation/build_array.py
@@ -6,9 +6,17 @@ class Solution():
         # method 1 brute method 
         # Runtime: 267 ms, faster than 5.25% of Python3 online submissions for Build Array from Permutation.
         # Memory Usage: 14.2 MB, less than 7.31% of Python3 online submissions for Build Array from Permutation.
+        '''
         ret = []
         for index in range(0 ,len(nums)):
             ret.append(nums[nums[index]])
 
         return ret
+        '''
 
+        # method 2 using one line
+        # Runtime: 165 ms, faster than 47.71% of Python3 online submissions for Build Array from Permutation.
+        # Memory Usage: 14.2 MB, less than 7.31% of Python3 online submissions for Build Array from Permutation.
+        
+        ret = [nums[nums[index]] for index in range(0,len(nums))]
+        return ret
