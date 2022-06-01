681c323e54c9ac145d29655e9737fa8af33f14dc
xiao-bo <ay852852@gmail.com>
2022-06-01 22:45:20 +0800

    feature: 修改一下排版

--- easy/array/NAaryTreePreorderTraversal/n_ary_tree_preorder_traversal.py
+++ easy/array/NAaryTreePreorderTraversal/n_ary_tree_preorder_traversal.py
@@ -12,6 +12,7 @@ class Solution:
         # using recursion method
         # Runtime: 50 ms, faster than 91.53% of Python3 online submissions for N-ary Tree Preorder Traversal.
         # Memory Usage: 16.4 MB, less than 13.77% of Python3 online submissions for N-ary Tree Preorder Traversal.
+        
         stack = []
         if root:
             stack.append(root.val)
@@ -20,3 +21,4 @@ class Solution:
                     stack.extend(self.preorder(children))
 
         return stack
+        
