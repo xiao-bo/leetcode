4cedf87e56aef103f10b1d3d546b2cbc3e4c0e7a
xiao-bo <ay852852@gmail.com>
2022-06-03 17:34:14 +0800

    refactor: 重構，拆成兩個function，變乾淨，但記憶體用量佔更多

--- array/medium/NumberofLaserBeamsinaBank/number_of_laser_beams_in_a_bank.py
+++ array/medium/NumberofLaserBeamsinaBank/number_of_laser_beams_in_a_bank.py
@@ -5,11 +5,27 @@ class Solution:
     def numberOfBeams(self, bank: List[str]) -> int:
         # brute method is O(mn)
 
-        # using math to solve, time complexity is
+        # using math to solve, time complexity is O(nm+m)without refactor
         # Runtime: 334 ms, faster than 25.59% of Python3 online submissions for Number of Laser Beams in a Bank.
         # Memory Usage: 15.9 MB, less than 89.93% of Python3 online submissions for Number of Laser Beams in a Bank.
 
-        number_of_beams = 0
+        
+        # refactor 
+        
+        # Runtime: 305 ms, faster than 29.63% of Python3 online submissions for Number of Laser Beams in a Bank.
+        # Memory Usage: 16.1 MB, less than 48.35% of Python3 online submissions for Number of Laser Beams in a Bank.
+        
+        count_of_1_of_row = \
+            self.__calculate_count_of_1_each_row(bank)
+        
+        number_of_beams = \
+            self.__calculate_number_of_beams(count_of_1_of_row)
+        
+        return number_of_beams
+
+
+
+    def __calculate_count_of_1_each_row(self, bank):
         count_of_1_of_row = []
         # calculate count of 1 of each row
         for row in bank:
@@ -17,13 +33,21 @@ class Solution:
             for char in row:
                 if char == '1':
                     tmp = tmp + 1
-
             if tmp != 0:
                 count_of_1_of_row.append(tmp)
 
+        return count_of_1_of_row
+
+    def __calculate_number_of_beams(self, counts):
         # calculate number_of_beams
-        for index in range(0, len(count_of_1_of_row)-1):
+        number_of_beams = 0
+        for index in range(0, len(counts)-1):
             number_of_beams += \
-                count_of_1_of_row[index] * count_of_1_of_row[index+1]
+                counts[index] * counts[index+1]
 
         return number_of_beams
+
+
+
+    def __calculate_count_of_1_each_row_2(self, bank):
+        
