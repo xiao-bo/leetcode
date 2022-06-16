10848ad5eabfd72d1b657640e6aa9e1fdf5066e4
xiao-bo <ay852852@gmail.com>
2022-06-16 23:46:11 +0800

    feature: 提升暴力法，速度快了三倍，但我相信還有更快的方法

--- array/easy/TimeNeededtoBuyTickets/test_time_needed_to_buy_tickets.py
+++ array/easy/TimeNeededtoBuyTickets/test_time_needed_to_buy_tickets.py
@@ -3,26 +3,21 @@ from time_needed_to_buy_tickets import Solution
 def test_solution():
 
     s = Solution()
-    tickets = [2,3,2]
-    k = 2
-
-    assert s.timeRequiredToBuy(tickets, 0) == 4
-    assert s.timeRequiredToBuy(tickets, 1) == 7
-    assert s.timeRequiredToBuy(tickets, 2) == 6
+    
+    assert s.timeRequiredToBuy([2,3,2], 0) == 4
+    assert s.timeRequiredToBuy([2,3,2], 1) == 7
+    assert s.timeRequiredToBuy([2,3,2], 2) == 6
 
     
-    tickets = [5,1,1,1]
     
-    assert s.timeRequiredToBuy(tickets, 0) == 8
-    assert s.timeRequiredToBuy(tickets, 1) == 2
+    assert s.timeRequiredToBuy([5,1,1,1], 0) == 8
+    assert s.timeRequiredToBuy([5,1,1,1], 1) == 2
     
 
-
-    tickets = [5,1,2,3,4]
     
-    assert s.timeRequiredToBuy(tickets, 0) == 15
-    assert s.timeRequiredToBuy(tickets, 1) == 2
-    assert s.timeRequiredToBuy(tickets, 2) == 7
-    assert s.timeRequiredToBuy(tickets, 3) == 11
-    assert s.timeRequiredToBuy(tickets, 4) == 14
+    assert s.timeRequiredToBuy([5,1,2,3,4], 0) == 15
+    assert s.timeRequiredToBuy([5,1,2,3,4], 1) == 2
+    assert s.timeRequiredToBuy([5,1,2,3,4], 2) == 7
+    assert s.timeRequiredToBuy([5,1,2,3,4], 3) == 11
+    assert s.timeRequiredToBuy([5,1,2,3,4], 4) == 14
     
--- array/easy/TimeNeededtoBuyTickets/time_needed_to_buy_tickets.py
+++ array/easy/TimeNeededtoBuyTickets/time_needed_to_buy_tickets.py
@@ -1,28 +1,48 @@
 from typing import List
-import queue
+
 class Solution:
     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
         # brute method 
         # Runtime: 386 ms, faster than 5.80% of Python3 online submissions for Time Needed to Buy Tickets.
         # Memory Usage: 14.2 MB, less than 16.02% of Python3 online submissions for Time Needed to Buy Tickets.
+        '''
         t = tickets.copy()
         q = queue.Queue()
 
-        for x in t:
+        for x in tickets:
             q.put(x)
         second = 0
         index = 0
-        len_queue = len(t)
-        while t[k] != 0:
-            print(t)
-            if t[index] <=0:
+        len_queue = len(tickets)
+        while tickets[k] != 0:
+            if tickets[index] <=0:
                 index = (index+1)%len_queue
                 continue
             
-            t[index] -= 1
+            tickets[index] -= 1
             second +=1
             index = (index+1)%len_queue
-            current = q.get()
-            q.put(current)
+            current = q.pop(0)
+            q.append(current)
+
+        return second
+        '''
+        ## improve brute method 
+        # Runtime: 136 ms, faster than 5.80% of Python3 online submissions for Time Needed to Buy Tickets.
+        # Memory Usage: 13.9 MB, less than 16.02% of Python3 online submissions for Time Needed to Buy Tickets.
+        second = 0
+        index = 0
+        len_queue = len(tickets)
+        q = tickets.copy()
+        while tickets[k] != 0:
+            if tickets[index] <=0:
+                index = (index+1)%len_queue
+                continue
+            
+            tickets[index] -= 1
+            second +=1
+            index = (index+1)%len_queue
+            current = q.pop(0)
+            q.append(current)
 
         return second
