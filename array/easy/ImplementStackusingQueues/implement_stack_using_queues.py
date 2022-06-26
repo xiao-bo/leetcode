30eecea1bd14dda77abf040a1682731dae31fec2
xiao-bo <ay852852@gmail.com>
2022-06-26 19:17:22 +0800

    feature: 使用top_element記錄top of stack，結果反而變慢XD

--- array/easy/ImplementStackusingQueues/implement_stack_using_queues.py
+++ array/easy/ImplementStackusingQueues/implement_stack_using_queues.py
@@ -4,13 +4,20 @@ class MyStack:
 
     # Runtime: 39 ms, faster than 65.76% of Python3 online submissions for Implement Stack using Queues.
     # Memory Usage: 13.9 MB, less than 72.44% of Python3 online submissions for Implement Stack using Queues.
+
+    # using top_element to record top of stack
+    # Runtime: 61 ms, faster than 9.81% of Python3 online submissions for Implement Stack using Queues.
+    # Memory Usage: 14 MB, less than 23.27% of Python3 online submissions for Implement Stack using Queues.
     
+
     def __init__(self):
         self.q1 = []
         self.q2 = []
-
+        self.top_element = 0
     def push(self, x: int) -> None:
         self.q1.append(x)
+
+        self.top_element = x
 
     def pop(self) -> int:
 
@@ -18,7 +25,9 @@ class MyStack:
             return None
 
         while len(self.q1) > 1:
-            self.q2.append(self.q1.pop(0))
+            element = self.q1.pop(0)
+            self.q2.append(element)
+            self.top_element = element
 
         ret = self.q1.pop(0)
 
@@ -29,21 +38,7 @@ class MyStack:
         return ret
 
     def top(self) -> int:
-
-        if len(self.q1) == 0:
-            return None
-
-        while len(self.q1) > 1:
-            self.q2.append(self.q1.pop(0))
-
-        ret = self.q1.pop(0)
-        self.q2.append(ret)
-
-        while len(self.q2) != 0:
-            # recorvery q1
-            self.q1.append(self.q2.pop(0))
-
-        return ret
+        return self.top_element
         
 
     def empty(self) -> bool:
