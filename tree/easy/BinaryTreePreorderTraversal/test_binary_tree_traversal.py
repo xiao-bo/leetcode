58ea433413e8d681bbec7b500034ecbf66c00bda
xiao-bo <ay852852@gmail.com>
2022-07-03 15:46:25 +0800

    feature: 測試拉出來

---
+++ tree/easy/BinaryTreePreorderTraversal/test_binary_tree_traversal.py
@@ -0,0 +1,54 @@
+from binary_tree_preorder_traversal import PreorderTraversal, TreeNode
+
+
+class TestTraversal(object):
+
+    def setup(self):
+        self.root1 = self._prepare_dataset_1()
+        self.root2 = self._prepare_dataset_2()
+        self.root3 = self._prepare_dataset_3()
+        self.root4 = self._prepare_dataset_4()
+
+    def _prepare_dataset_1(self):
+        Node3 = TreeNode(3)
+        Node2 = TreeNode(2, left=Node3, right=None)
+        root = TreeNode(1, left=None, right=Node2)
+
+        return root
+
+    def _prepare_dataset_2(self):
+        root = None
+        return root
+
+    def _prepare_dataset_3(self):
+        root = TreeNode(1)
+        return root
+
+    def _prepare_dataset_4(self):
+        Node6 = TreeNode(6)
+        Node5 = TreeNode(5, right=Node6)
+        Node4 = TreeNode(4)
+        Node3 = TreeNode(3)
+        Node2 = TreeNode(2, left=Node4, right=Node5)
+        root = TreeNode(1, left=Node2, right=Node3)
+        return root
+
+    def test_preorder(self):
+        preorder = PreorderTraversal()
+
+        ret = preorder.preorderTraversal(self.root1)
+
+        assert ret == [1,2,3]
+
+        ret = preorder.preorderTraversal(self.root2)
+
+        assert ret == []
+
+        ret = preorder.preorderTraversal(self.root3)
+
+        assert ret == [1]
+
+        ret = preorder.preorderTraversal(self.root4)
+
+        assert ret == [1,2,4,5,6,3]
+    
