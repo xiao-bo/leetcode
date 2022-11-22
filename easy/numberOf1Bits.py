1c4e760a2ce2f7565b4a5c118093885e6280b6e1
xiao-bo <ay852852@gmail.com>
2022-11-22 22:58:11 +0800

    feature: solve it by bit manipulation

---
+++ easy/numberOf1Bits.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        count = 0
+        tmp = n 
+        while tmp > 0:
+            if tmp % 2 == 1:
+                count +=1
+            tmp = tmp >> 1 
+                
+        return count
