08919df1dde1f0b26f819002baf7ff26edb262d9
bowen <bowen@leju.com.tw>
2024-02-07 14:21:22 +0800

    feature: solve threesum again

--- array/medium/threeSum/threeSum.py
+++ array/medium/threeSum/threeSum.py
@@ -26,3 +26,28 @@ class Solution:
                     third = third - 1
 
         return list(set(result))
+
+        # two pointer again
+        # Runtime 1488 ms Beats 21.77% of users with Python3
+        # Memory 177.35 MB Beats 5.09% of users with Python3
+        nums.sort()
+        result = []
+        for t in range(0, len(nums)):
+            i = 0
+            j = len(nums) - 1
+            while i != t and j != t and i < j:
+                #print(i,j,t)
+                tmp = nums[i] + nums[j]
+                if tmp > -nums[t]:
+                    j = j - 1
+                elif tmp < -nums[t]:
+                    i = i + 1
+                elif tmp == -nums[t]:
+                    result.append((
+                        nums[i],
+                        nums[j],
+                        nums[t]
+                    ))
+                    i = i + 1
+                    j = j - 1
+        return list(set(result))
