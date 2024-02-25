4e77435ffbf20d683dd7a9c32abf6d4d476654e8
bowen <bowen@leju.com.tw>
2024-02-25 18:05:10 +0800

    feature: solve evaluate reverse polish notation

--- array/medium/EvaluateReversePolishNotation/evaluateReversePolishNotation.py
+++ array/medium/EvaluateReversePolishNotation/evaluateReversePolishNotation.py
@@ -29,3 +29,33 @@ class Solution:
                 s.append(char)
 
         return int(s.pop(-1))
+
+    def evalRPN_2024(self, tokens: List[str]) -> int:
+        # stack method in 2024 year
+        # Runtime 61ms Beats 83.60% of users with Python3
+        # Memory 17.16MB Beats 46.12% of users with Python3
+        number_stack = []
+
+        a = ['+', '-', '*', '/']
+        for c in tokens:
+            if c.lstrip('-+').isdigit():
+                number_stack.append(int(c))
+            elif c in a:
+                cur = number_stack.pop()
+                pre = number_stack.pop()
+                tmp = self.__str_to_int(cur, pre, c)
+                number_stack.append(tmp)
+
+        return number_stack.pop()
+
+    def __str_to_int(self, cur: int, pre: int, c: str) -> int:
+        res = 0
+        if c == "+":
+            res = pre + cur
+        elif c == "-":
+            res = pre - cur
+        elif c == "*":
+            res = pre * cur
+        elif c == "/":
+            res = int(pre / cur)
+        return res
