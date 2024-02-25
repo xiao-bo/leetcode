ad9c338dd163f45f3dd1f1623bf77f76cdc1e6b1
bowen <bowen@leju.com.tw>
2024-02-25 13:56:47 +0800

    style: ruff

--- array/medium/minStack.py
+++ array/medium/minStack.py
@@ -3,7 +3,7 @@ class MinStack_2023:
     # Runtime63 ms Beats 70.1% Memory18.4 MB Beats 11.87%
     def __init__(self):
         self.minstack = []
-        self.min = float('inf')
+        self.min = float("inf")
 
     def push(self, val: int) -> None:
         if len(self.minstack) == 0:
@@ -11,7 +11,6 @@ class MinStack_2023:
             # 把當前最小值存起來
             self.minstack.append((val, val))
         else:
-
             self.min = min(val, self.min)
             self.minstack.append((val, self.min))
 
@@ -20,7 +19,7 @@ class MinStack_2023:
         if len(self.minstack) > 0:
             self.min = self.minstack[-1][1]
         else:
-            self.min = float('inf')
+            self.min = float("inf")
 
     def top(self) -> int:
         return self.minstack[-1][0]
