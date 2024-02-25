610cf31a9e92b42cd7ac563087bbb53b1dd3d547
bowen <bowen@leju.com.tw>
2024-02-25 18:57:22 +0800

    docs: 新增註解

--- linked_list/LinkedListCycle.py
+++ linked_list/LinkedListCycle.py
@@ -4,8 +4,12 @@
 #         self.val = x
 #         self.next = None
 
+
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
+        # 2022 solution
+        # Runtime 58ms Beats 14.97% of users with Python3
+        # Memory 17.27MB Beats 99.99% of users with Python3
         if not head:
             return False
         cur = head
@@ -17,4 +21,3 @@ class Solution:
                 cur = cur.next
 
         return False
-                
