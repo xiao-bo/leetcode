fd15687f245337e132c969c725f3b3dda8a61c63
bowen <bowen@leju.com.tw>
2024-03-18 21:14:17 +0800

    feature: solve invert binary tree again

--- tree/easy/InvertBinaryTree/invertBinaryTree.py
+++ tree/easy/InvertBinaryTree/invertBinaryTree.py
@@ -15,7 +15,7 @@ class Solution(object):
         
         #Runtime: 16 ms, faster than 74.14% of Python online submissions for Invert Binary Tree.
         #Memory Usage: 11.9 MB, less than 25.00% of Python online submissions for Invert Binary Tree.
-        ## recursion method
+        ## recursion method in 2022
         '''
         if root == None:
             return None
@@ -26,9 +26,10 @@ class Solution(object):
             return root
         '''
 
-        ## iteration method
+        ## iteration method in 2022
         ## Runtime: 12 ms, faster than 93.55% of Python online submissions for Invert Binary Tree.
         ## Memory Usage: 11.8 MB, less than 57.50% of Python online submissions for Invert Binary Tree.
+        """
         stack = [root]
         
         
@@ -38,6 +39,30 @@ class Solution(object):
                 node.right,node.left = node.left, node.right
                 stack.append(node.left)
                 stack.append(node.right)
+        return root
+        """
+
+        # recursion method in 2024 
+        # Runtime 35ms Beats 57.69% of users with Python3
+ i      # Memory 16.51MB Beats 37.39% of users with Python3
+        if root is None:
+            return root
+
+        if root.left and root.right:
+            # 左右都有值
+            root.left, root.right = root.right, root.left
+            root.left = self.invertTree(root.left)
+            root.right = self.invertTree(root.right)
+
+        elif root.left and not root.right:
+            root.right = root.left
+            root.left = None
+            root.right = self.invertTree(root.right)
+        elif not root.left and root.right:
+            root.left = root.right
+            root.right = None
+            root.left = self.invertTree(root.left)
+
         return root
 
     def printLVR(self,root):
